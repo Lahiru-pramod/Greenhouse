5cdadd8802e68599926acf51fd44be8b8a4168fa
lahiru Pramod <lahirupramod41@gmail.com>
2021-08-18 09:51:42 +0530

    Edit Signed-off-by: lahiru Pramod <lahirupramod41@gmail.com>

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,16 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+
+
 function Navbar() {
+
+  const [click,setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+
+
   return (
   <>
   <nav className='avbar'>
@@ -8,8 +18,33 @@ function Navbar() {
 
         <Link to='/' className='navbar-logo'>
 
-            Greenhouse.lk <i className='fab fa-typo3'/>
+        <i class="fa fa-home" aria-hidden="true"></i>Greenhouse.lk
         </Link>
+        <div className='menu-icon' onClick={handleClick}>
+
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+
+          <li> 
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              home
+            </Link>
+          </li>
+          <li> 
+            <Link to='/plants' className='nav-links' onClick={closeMobileMenu}>
+              Plants
+            </Link>
+          </li>
+          <li> 
+            <Link to='/aboutus' className='nav-links' onClick={closeMobileMenu}>
+              About us
+            </Link>
+          </li>
+
+
+        </ul>
       </div>
 
   </nav>
