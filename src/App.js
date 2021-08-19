befbb8861f65e23faf47512b46d2f5b8042bed72
lahiru Pramod <lahirupramod41@gmail.com>
2021-08-19 12:38:39 +0530

    add Signed-off-by: lahiru Pramod <lahirupramod41@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 import React from 'react';
-import Navbar from './Components/Navbar';
+import Navbar from './Components/Home/Navbar';
 import {BrowserRouter as Router, Switch, Route}from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    
         <Router>
+           <Navbar/>
+          <div className="container-fluid">
+          <div className="row">
+            <div className="col">
+               Lahiru
 
-      <Navbar />
+        </div>
+
+
+      </div>
+      </div>
         </Router>
-
-    </>
+       
+    
   );
 }
 
--- src/Components/Home/Navbar.js
+++ src/Components/Home/Navbar.js
@@ -1,26 +1,27 @@
 import React, {Component} from 'react';
+ import './navbar.css';
 
-// import './navbar.css';
+ 
 class App extends Component{
   render() {
     return (
 
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container-fluid">
-        <a className="navbar-brand" href="#"><i class="fa fa-home" aria-hidden="true"></i>Navbar</a>
+        <a className="navbar-brand fw-bold fs-2 text" href="#"><i class="fa fa-home" aria-hidden="true"></i>Greenhouse</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <a className="nav-link active text-light fs-4" aria-current="page" href="#">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
+                <a className="nav-link active text-light fs-4" href="#">Plants</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link disabled" href="#" tabIndex={-1} aria-disabled="true">Disabled</a>
+                <a className="nav-link active text-light fs-4" aria-current="page" href="#">About us</a>
               </li>
             </ul>
             <form className="d-flex">
