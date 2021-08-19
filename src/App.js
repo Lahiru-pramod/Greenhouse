edc3f6c16ed08a773af6d699136641dcd48e0168
lahiru Pramod <lahirupramod41@gmail.com>
2021-08-19 11:24:07 +0530

    new Signed-off-by: lahiru Pramod <lahirupramod41@gmail.com>

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ function App() {
     <>
         <Router>
 
-        <Navbar />
-        <Switch>
-          <Route path='/' exact />
-        </Switch>
+      <Navbar />
         </Router>
 
     </>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,57 +1,37 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
+import React, {Component} from 'react';
 
+// import './navbar.css';
+class App extends Component{
+  render() {
+    return (
 
-function Navbar() {
-
-  const [click,setClick] = useState(false);
-
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
-
-
-
-  return (
-  <>
-  <nav className='avbar'>
-      <div className='navbar-container'>
-
-        <Link to='/' className='navbar-logo'>
-
-        <i class="fa fa-home" aria-hidden="true"></i>Greenhouse.lk
-        </Link>
-        <div className='menu-icon' onClick={handleClick}>
-
-          <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
-
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+        <a className="navbar-brand" href="#"><i class="fa fa-home" aria-hidden="true"></i>Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" href="#" tabIndex={-1} aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
         </div>
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-
-          <li> 
-            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-              home
-            </Link>
-          </li>
-          <li> 
-            <Link to='/plants' className='nav-links' onClick={closeMobileMenu}>
-              Plants
-            </Link>
-          </li>
-          <li> 
-            <Link to='/aboutus' className='nav-links' onClick={closeMobileMenu}>
-              About us
-            </Link>
-          </li>
-
-
-        </ul>
-      </div>
-
-  </nav>
-
-</>
-
-  );
+      </nav>
+    );
+  }
 }
+export default App;
 
-export default Navbar ;
